4efd6231780849d88b254bf2d6ed3fb66643492c
Awais_Bro <AWAISAKHTAR270@IITKGP.AC.IN>
2020-12-11 23:13:53 +0530

    Node js server working

--- app.js
+++ app.js
@@ -1,79 +1,120 @@
 var express = require('express');
-// var bodyParser = require('body-parser');
 var app = express();
 var request = require("request")
-app.set('view engine','ejs');
 
-app.get('/actionML',function(req,res){
-    res.render('index',{qs:req.query});
+app.get('/reportview',function(req,res){  // <- Recieve request like http://localhost:3000/reportview?userId=3123&page=44342
+    console.log(req.query);
+    var isoDate = new Date().toISOString()
+    console.log(isoDate);
+    //events
+
+    var data1 = { 
+        "event": "read",
+        "entityType": "user",
+        "entityId": "Rando Norris",
+        "targetEntityType": "item",
+        "targetEntityId": req.query.page,
+        "eventTime": isoDate,  
+    }
+
+    console.log(data1);
+
+    url = "http://localhost:9090/engines/test/events"
+
+    request({
+        url: url,
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+            },
+        json: data1,
+    }, function (error, response, body) {
+        if (!error && response.statusCode === 201) { // created
+            console.log(body)
+            console.log("response.statusCode: " + response.statusCode)
+            console.log("response.statusText: " + response.statusMessage)
+        }
+        else {
+
+            console.log("error: " + error)
+            console.log("response.statusCode: " + response.statusCode)
+            console.log("response.statusText: " + response.statusMessage)
+        }
+    })
+
+    var data2 = {
+        "event": "$set",
+        "entityItem": "item",
+        "entityId": req.query.page,  
+        "properties": {
+             "category" : ["elctronics","covid"]
+         },
+         "eventTime": isoDate
+    }
+
+    console.log(data2);
+
+    request({
+        url: url,
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+            },
+        json: data2,
+    }, function (error, response, body) {
+        if (!error ) { // created
+            console.log(body)
+            console.log("response.statusCode: " + response.statusCode)
+            console.log("response.statusText: " + response.statusMessage)
+        }
+        else {
+
+            console.log("error: " + error)
+            console.log("response.statusCode: " + response.statusCode)
+            console.log("response.statusText: " + response.statusMessage)
+        }
+    })
+
+
+    // -> Train before query
+
+    request({
+        url: "http://localhost:9090/engines/test/jobs",
+        method: "POST",
+        json: {},
+    }, function (error, response, body) {
+        if (!error ) {
+            console.log(body)
+            console.log("response.statusCode: " + response.statusCode)
+            console.log("response.statusText: " + response.statusMessage)
+        }
+        else {
+    
+            console.log("error: " + error)
+        }
+    });
+
+    // -> Query 
+
+    request({
+        url: "http://localhost:9090/engines/test/queries",
+        method: "POST",
+        json: {
+            "user" : "Rando Norris"
+        },
+    }, function (error, response, body) {
+        if (!error ) {
+            console.log(body)
+            console.log("response.statusCode: " + response.statusCode)
+            console.log("response.statusText: " + response.statusMessage)
+        }
+        else {
+
+            console.log("error: " + error)
+        }
+    })
+    res.end(JSON.stringify(data1)+JSON.stringify(data2));
 });
-
-// -> events
-
-var data1 = { 
-    "event": "read",
-    "entityType": "user",
-    "entityId": "Rando Norris",
-    "targetEntityType": "item",
-    "targetEntityId": "6575",
-    "eventTime": "2019-10-05T21:02:49.228Z",  
-}
-
-url = "http://localhost:9090/engines/tp_ur/events"
-
-request({
-    url: url,
-    method: "POST",
-    headers: {
-        "content-type": "application/json",
-        },
-    json: data1,
-}, function (error, response, body) {
-    if (!error && response.statusCode === 201) { // created
-        console.log(body)
-        console.log("response.statusCode: " + response.statusCode)
-        console.log("response.statusText: " + response.statusMessage)
-    }
-    else {
-
-        console.log("error: " + error)
-        console.log("response.statusCode: " + response.statusCode)
-        console.log("response.statusText: " + response.statusMessage)
-    }
-})
-
-data2 = {
-    "event": "$set",
-    "entityItem": "item",
-    "entityId": ["54523","6868"],   
-    "properties": {
-         "category" : ["elctronics", "covid"]
-     },
-     "eventTime": "2019-10-05T21:02:49.228Z" 
-
-}
-
-request({
-    url: url,
-    method: "POST",
-    headers: {
-        "content-type": "application/json",
-        },
-    json: data2,
-}, function (error, response, body) {
-    if (!error ) { 
-        console.log(body)
-        console.log("response.statusCode: " + response.statusCode)
-        console.log("response.statusText: " + response.statusMessage)
-    }
-    else {
-
-        console.log("error: " + error)
-        console.log("response.statusCode: " + response.statusCode)
-        console.log("response.statusText: " + response.statusMessage)
-    }
-})
-
 
 // -> Empty json for training
 var time = 0;
@@ -81,7 +122,7 @@ var timer = setInterval(function(){
     console.log(time + "seconds ")
     time += 3;
     request({
-        url: "http://localhost:9090/engines/tp_ur/jobs",
+        url: "http://localhost:9090/engines/test/jobs",
         method: "POST",
         json: {},
     }, function (error, response, body) {
@@ -96,27 +137,6 @@ var timer = setInterval(function(){
         }
     })    
 },3000);
-
-// -> Query 
-
-request({
-    url: "http://localhost:9090/engines/tp_ur/queries",
-    method: "POST",
-    json: {
-        "user" : "Spark job"
-    },
-}, function (error, response, body) {
-    if (!error ) {
-        console.log(body)
-        console.log("response.statusCode: " + response.statusCode)
-        console.log("response.statusText: " + response.statusMessage)
-    }
-    else {
-
-        console.log("error: " + error)
-    }
-})
-
 
 
 app.listen(3000);
